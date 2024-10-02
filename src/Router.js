7c3e4d8256c2b5726638eb8b1555463f7ebe0741
김가희 <bn8245@gmail.com>
2024-10-02 11:55:53 +0900

    수정하기 & 삭제하기 버튼 완료/드롭다운 수정 중

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import QuestionListPage from "./pages/QuestionListPage/QuestionListPage";
 
 function Router() {
-  return (
-    <BrowserRouter>
-        <Routes>
-          
-        </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/questionlist" element={<QuestionListPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 export default Router;
