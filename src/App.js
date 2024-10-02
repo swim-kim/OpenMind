1a7c1d053edc138ce81e3e5e0b09620ddb8a75db
나승엽 <a01031928263@gmail.com>
2024-10-02 17:57:08 +0900

    공통 컴포넌트 배포

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import './App.css';
-import Router from "./Router";
+import React, { useEffect } from "react";
+import "./styles/global.css";
+import {
+  FacebookShareButton,
+  KakaoShareButton,
+  LinkShareButton,
+} from "./components/Buttonshare/Buttonshare.jsx";
+import { FloatingButton } from "./components/Buttonfloating/Buttonfloating.jsx";
 
 function App() {
+  useEffect(() => {
+    if (window.Kakao) {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init("4efde28e8b63fd9c48dfdf697ecbf6d6");
+        console.log("카카오 SDK 초기화 완료");
+      } else {
+        console.log("Kakao SDK는 이미 초기화되었습니다.");
+      }
+    } else {
+      console.error("Kakao SDK가 로드되지 않았습니다.");
+    }
+  }, []);
+
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: "01031928263",
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v15.0",
+      });
+    };
+  }, []);
   return (
-    <>
-      <Router />
-    </>
+    <div>
+      <KakaoShareButton />
+      <FacebookShareButton />
+      <LinkShareButton />
+      <FloatingButton />
+    </div>
   );
 }
 
