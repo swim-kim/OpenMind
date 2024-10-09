84339f671f29de9b83cf78d2e9276d84a18ff300
김가희 <bn8245@gmail.com>
2024-10-09 19:55:11 +0900

    feat: 홈페이지 등 경로 이동 기능 구현

--- src/pages/MainPage/Main.jsx
+++ src/pages/MainPage/Main.jsx
@@ -1,92 +1,93 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { subjects_create } from "../../api/storage/swagger/Subjects";
-import { ReactComponent as LogoSVG } from "../../assets/icon/logo.svg";
+import { subjects_create } from "../../api/swagger/Subjects";
+import { ReactComponent as DefaultLogo } from "../../assets/default/defaultLogo.svg";
 import { ReactComponent as PersonIconSVG } from "../../assets/icon/Person.svg";
 import { ReactComponent as ArrowRightIconSVG } from "../../assets/icon/arrow-right.svg";
-import { ReactComponent as BackgroundSVG } from "../../assets/icon/v872batch5-nunny-04 1.svg";
+import { ReactComponent as BackgroundSVG } from "../../assets/figma.named.svg/v-872-batch-5-nunny-04-1.svg";
 
 import {
-  Container,
-  FormContainer,
-  InputContainer,
-  Input,
-  GoToQuestionsButton,
-  GoToAnswerButton,
-} from "./Main.styles";
+	Container,
+	FormContainer,
+	InputContainer,
+	Input,
+	GoToQuestionsButton,
+	GoToAnswerButton,
+} from "./MainStyle";
 
 import LocalStore from "../../api/storage/LocalStore";
 
 const Main = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [subject, setSubject] = useState(
-    JSON.parse(LocalStore.getItem("subject"))
-  );
-  const [name, setName] = useState("");
-  const [team] = useState("10-1");
+	const [subject, setSubject] = useState(
+		JSON.parse(LocalStore.getItem("subject"))
+	);
+	const [name, setName] = useState("");
+	const [team] = useState("10-1");
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
+	const handleNameChange = (event) => {
+		setName(event.target.value);
+	};
 
-  const createSubject = async () => {
-    try {
-      const res = await subjects_create(team, name);
-      if (res !== null && res !== undefined) {
-        LocalStore.setItem("subject", JSON.stringify(res));
-        setSubject(res);
-        return res;
-      }
-    } catch (error) {
-      console.error("Error creating subject:", error);
-    }
-    return null;
-  };
+	const createSubject = async () => {
+		try {
+			const res = await subjects_create(team, name);
+			if (res !== null && res !== undefined) {
+				LocalStore.setItem("subject", JSON.stringify(res));
+				setSubject(res);
+				console.log(res);
+				return res;
+			}
+		} catch (error) {
+			console.error("Error creating subject:", error);
+		}
+		return null;
+	};
 
-  const onClickAnswer = async () => {
-    if (name.trim() === "" && subject?.name === undefined) {
-      alert("Enter name and Click the button.");
-      return;
-    } else if (name.trim().length > 0) {
-      const res = await createSubject();
-      if (res) {
-        setSubject(res);
-        navigate("/post/answer/WriteAnswer");
-      }
-    } else if (subject?.name !== undefined) {
-      navigate("/post/answer/WriteAnswer");
-    }
-  };
-const onClickListSubjects = () => {
-    navigate("/list/ListSubjects");
-  };
+	const onClickAnswer = async () => {
+		if (name.trim() === "" && subject?.name === undefined) {
+			alert("Enter name and Click the button.");
+			return;
+		} else if (name.trim().length > 0) {
+			const res = await createSubject();
+			if (res) {
+				setSubject(res);
+				navigate("/post/answer/WriteAnswer");
+			}
+		} else if (subject?.name !== undefined) {
+			navigate("/post/answer/WriteAnswer");
+		}
+	};
+	const onClickListSubjects = () => {
+		navigate("/list/ListSubjects");
+	};
 
-  return (
-    <Container>
-      <LogoSVG />
-      <FormContainer>
-        <InputContainer>
-          <PersonIconSVG />
-          <Input
-            id={name}
-            value={name}
-            onChange={handleNameChange}
-            type="text"
-            placeholder="이름을 입력하세요"
-          />
-        </InputContainer>
-        <GoToAnswerButton onClick={onClickAnswer} disabled={!name.trim()}>
-          질문 받기
-        </GoToAnswerButton>
-      </FormContainer>
-      <GoToQuestionsButton onClick={onClickListSubjects}>
-        질문하러 가기
-        <ArrowRightIconSVG />
-      </GoToQuestionsButton>
-      <BackgroundSVG />
-    </Container>
-  );
+	return (
+		<Container>
+			<DefaultLogo />
+			<FormContainer>
+				<InputContainer>
+					<PersonIconSVG />
+					<Input
+						id={name}
+						value={name}
+						onChange={handleNameChange}
+						type="text"
+						placeholder="이름을 입력하세요"
+					/>
+				</InputContainer>
+				<GoToAnswerButton onClick={onClickAnswer} disabled={!name.trim()}>
+					질문 받기
+				</GoToAnswerButton>
+			</FormContainer>
+			<GoToQuestionsButton onClick={onClickListSubjects}>
+				질문하러 가기
+				<ArrowRightIconSVG />
+			</GoToQuestionsButton>
+			<BackgroundSVG />
+		</Container>
+	);
 };
 
 export default Main;
--- src/pages/QuestionListPage/components/header/Headers.jsx
+++ src/pages/QuestionListPage/components/header/Headers.jsx
@@ -1,7 +1,7 @@
-import {ReactComponent as DefaultLogo} from "../../../../assets/default/defaultLogo.svg";
+import { ReactComponent as DefaultLogo } from "../../../../assets/default/defaultLogo.svg";
 import style from "styled-components";
 import CustomButton from "../../../../components/CustomButton";
-
+import LocalStore from "../../../../api/storage/LocalStore";
 
 const StyleBox = style.div`
   display: flex;
@@ -24,17 +24,32 @@ const StyleBox = style.div`
 	};
 `;
 
+const ContainHeader = () => {
+	const handleTo = () => {
+		const subject = LocalStore.getItem("subject");
+		const parsedSubject = JSON.parse(subject);
+		// JSON은 객체처럼 생겼는데, 그냥 데이터 쪼가리
+		// LocalStorage는 key,value만 구성할수있고, value에는 숫자나, 문자열만 들어갈수있어요.
+		// JSON.stringify 함수를 통해서 문자열처럼 다루겠다. 하는 식으로 타입변환을 했다.
 
-const ContainHeader = ()=>{
-  const handleTo = ()=>{}
-  return(
-    <>
-      <StyleBox>
-        <DefaultLogo/>
-        <CustomButton mode="goToReply" onClick={handleTo}/>
-      </StyleBox>
-    </>
-  )
-}
+
+		window.location.href = `/post/${parsedSubject.id}/answer`;
+		// 로컬 스토리지에서 가져온다 값을
+		// id부분은 `${id}`이런식으로 리터럴 문법을 쓴다.
+		// www.naver.com/main
+		// /post/{id}/answer
+	};
+	const handleToHome = () => {
+		window.location.href = "/";
+	};
+	return (
+		<>
+			<StyleBox>
+				<DefaultLogo onClick={handleToHome} />
+				<CustomButton mode="goToReply" onClick={handleTo} />
+			</StyleBox>
+		</>
+	);
+};
 
 export default ContainHeader;
