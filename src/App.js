ff20ac90522cc5805b759c6b2574c35be1d41b14
swimkim <81215099+swim-kim@users.noreply.github.com>
2024-10-11 12:54:42 +0900

    fix: 키값 숨김

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   useEffect(() => {
     if (window.Kakao) {
       if (!window.Kakao.isInitialized()) {
-        window.Kakao.init("4efde28e8b63fd9c48dfdf697ecbf6d6");
+        window.Kakao.init(process.env.REACT_APP_KAKKAO);
         console.log("카카오 SDK 초기화 완료");
       } else {
         console.log("Kakao SDK는 이미 초기화되었습니다.");
@@ -19,14 +19,19 @@ function App() {
   useEffect(() => {
     window.fbAsyncInit = function () {
       window.FB.init({
-        appId: 3819506264969417,
-        xfbml: true,
-        version: 'v21.0',
+        appId: process.env.REACT_APP_FACEBOOK,
       });
     };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   }, []);
   
-
   return (
     <div>
       <Router />
